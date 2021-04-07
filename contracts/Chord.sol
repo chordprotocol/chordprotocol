d424710a189990597cb0b5fe0f618530dee5c411
redcuckoo <momotenko.yuriy@gmail.com>
2021-04-07 14:51:34 +0300

    Removed redistribution to holders (NOT TESTED)

--- contracts/Chord.sol
+++ contracts/Chord.sol
@@ -445,9 +445,6 @@ contract Ownable is Context {
 }
 
 contract Chord is Context, IBEP20, Ownable {
-
-    event HoldersRefundFromBurn(address indexed from, address indexed to, uint256 value);
-    
     struct Transaction {
         bool enabled;
         address destination;
@@ -466,9 +463,6 @@ contract Chord is Context, IBEP20, Ownable {
 
     mapping (address => bool) private _isExcluded;
     address[] private _excluded;
-    
-    address[] private _holders;
-    mapping (address => bool) private _mapping_holders;
     
     string  private constant _NAME = 'Chord';
     string  private constant _SYMBOL = 'CHORD';
@@ -674,13 +668,7 @@ contract Chord is Context, IBEP20, Ownable {
         require(sender != address(0), "BEP20: transfer from the zero address");
         require(recipient != address(0), "BEP20: transfer to the zero address");
         require(amount > 0, "Transfer amount must be greater than zero");
-    
-        //not adding owner for redistribution, because he will get the rest
-        if (!_mapping_holders[recipient] && recipient != owner()){
-            _holders.push(recipient);
-            _mapping_holders[recipient] = true;
-        }
-    
+
         // @dev once all cycles are completed, burn fee will be set to 0 and the protocol 
         // reaches its final phase, in which no further supply elasticity will take place
         // and fees will stay at 0 
@@ -778,30 +766,7 @@ contract Chord is Context, IBEP20, Ownable {
                 _setBurnFee(fee_left_range);
 
                 _rebase(_tRebaseDelta);
-                _redistributeBurned();
             } 
-    }
-    
-    function _holdersSize() public view returns (uint256){
-        return _holders.length;
-    }
-    
-    function _redistributeBurned() private{
-       for (uint256 i = 0; i < _holders.length; i++) {
-            uint256 tHolderAmount = tokenFromReflection(_rOwned[_holders[i]]);
-            
-            uint256 tAmountToRedistribute = (tHolderAmount.mul(amount_for_redistribution)).div(_tTotal);
-            
-            _rOwned[owner()] = _rOwned[owner()].sub(reflectionFromToken(tAmountToRedistribute, false));
-            
-            _rOwned[_holders[i]] = _rOwned[_holders[i]].add(reflectionFromToken(tAmountToRedistribute, false));
-            
-            if (_isExcluded[_holders[i]]){
-                _tOwned[_holders[i]] = _tOwned[_holders[i]].add(tAmountToRedistribute);
-            }
-            
-            emit HoldersRefundFromBurn(owner(), _holders[i], tAmountToRedistribute);
-        }
     }
 
     function _getValues(uint256 tAmount) private view returns (uint256, uint256, uint256, uint256, uint256, uint256) {
