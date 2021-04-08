45d23580e33a9a8c5f261273df3a7e6b2662b097
redcuckoo <momotenko.yuriy@gmail.com>
2021-04-08 12:26:27 +0300

    Updated license and package-lock

--- contracts/Chord.sol
+++ contracts/Chord.sol
@@ -1,5 +1,6 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.6.0;
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address payable) {
